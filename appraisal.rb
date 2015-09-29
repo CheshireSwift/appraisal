583e43bc578960d5e9fff3c51d1257eeeb140892
Sam Collard <sam.collard@softwire.com>
2015-09-29 22:58:37 +0100

    WIP

--- appraisal.rb
+++ appraisal.rb
@@ -1,5 +1,7 @@
 require_relative 'helpers'
 
+API_KEY = 'F5449F30-8B39-BB4E-9318-FCB0097BE9069B982DB5-5C6B-47A6-ADD5-55F354442BDB'
+
 class ItemRow
   attr_accessor :name
   attr_accessor :count
@@ -18,18 +20,22 @@ def array_to_hash(array)
   Hash[array.each_slice(2).to_a]
 end
 
-item_list = File.open(ARGV[0])
-ids_by_name = array_to_hash(item_list.map { |line| line.chomp.split(',') }.flatten)
-#puts ids_by_name
-bank_list = File.open(ARGV[1])
-prices = bank_list.map do |line| 
-  row = ItemRow.new(line)
-  id = ids_by_name[row.name]
-  if !id then
-    puts "No ID for name '#{row.name}'."
-    Price.new(0, 0)
-  else
-    get_value(id) * row.count
+if ARGV[0] then
+  item_list = File.open(ARGV[0])
+  ids_by_name = array_to_hash(item_list.map { |line| line.chomp.split(',') }.flatten)
+  #puts ids_by_name
+  bank_list = File.open(ARGV[1])
+  prices = bank_list.map do |line| 
+    row = ItemRow.new(line)
+    id = ids_by_name[row.name]
+    if !id then
+      puts "No ID for name '#{row.name}'."
+      Price.none
+    else
+      get_value(id) * row.count
+    end
   end
+else
+  prices = get_mats_value API_KEY
 end
 puts prices.inject(:+)
--- helpers.rb
+++ helpers.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'rack'
 
 BASE_URI = 'https://api.guildwars2.com/v2/'
 
@@ -18,23 +19,39 @@ class Price < Struct.new(:low, :high)
     "#{pretty_print self.low} - #{pretty_print self.high}"
   end
 
+  def Price.none
+    Price.new(0, 0)
+  end
 end
 
-def get(path)
-  uri = URI::join(BASE_URI, path)
+def get(path, query_hash = {})
+  query_string = query_hash.length > 0 ? '?' + Rack::Utils.build_query(query_hash) : ''
+  uri = URI::join(BASE_URI, path) + query_string
   response = Net::HTTP.get_response(uri)
   JSON.parse(response.body)
+end
+
+def get_mats_value(api_key)
+  bank_slots = get(
+    'account/materials',
+    {:access_token => api_key}
+  ).select { |bank_slot| bank_slot['count'] > 0 }
+  
+  bank_slots.each_with_index.map do |bank_slot, i|
+    STDOUT.write "#{i}/#{bank_slots.length} (#{(i.fdiv(bank_slots.length) * 100).round}%)\r"
+    get_value(bank_slot['id']) * bank_slot['count']
+  end
 end
 
 def get_value(item_id)
   if !item_id then
     puts "Missing entry."
-    return 0
+    return Price.none
   end
 
   results = get("commerce/prices/#{item_id}")
   #puts results
-  Price.new(results["buys"]["unit_price"], results["sells"]["unit_price"])
+  results['id'] ? Price.new(results["buys"]["unit_price"], results["sells"]["unit_price"]) : Price.none
 end
 
 def pretty_print(total_copper)
